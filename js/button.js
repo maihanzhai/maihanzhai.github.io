d5e3c67bd930d9af4df4f7596299c4d5be3a289b
maihanzhai <110963822+maihanzhai@users.noreply.github.com>
2023-05-26 13:11:49 +1000

    Add files via upload

---
+++ js/button.js
@@ -0,0 +1,14 @@
+// document.querySelector('.custom-btn').addEventListener('click', function () {
+
+//     Fancybox.bind("[data-fancybox]", {
+//         // Your custom options
+//     });
+
+//     submitButton.addEventListener('click', function () {
+//         alert('wahtever hahaha lol');
+//     });
+// });
+
+function showMessage() {
+    alert("Reservation successful! Thank you for your booking.");
+}
